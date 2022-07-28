5bc6fe9829894c3e4bb146e12ade6adccd991b67
Marcos Andrade <marcos_q_andrade@hotmail.com>
2022-07-28 20:06:07 -0300

    Add DepartmentDAO

---
+++ src/application/Program2.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.util.List;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		DepartmentDao departmentDao = DaoFactory.creatDepartmentDao();
+		
+		System.out.println("=== TEST 1: department findById ====");
+		Department department = departmentDao.findById(1);
+		
+		System.out.println(department);
+		
+		System.out.println("\n=== TEST 2: department findAll ====");
+		List<Department> list = departmentDao.findAll();
+		list.forEach(System.out::println);
+		
+	}
+
+}
---
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,111 @@
+package model.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import db.DbException;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+	
+	private Connection conn;
+
+	public DepartmentDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+
+	@Override
+	public void insert(Department obj) {
+		
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement("INSERT INTO department Name VALUES (?)", 
+					Statement.RETURN_GENERATED_KEYS);
+			
+			st.setString(2, null);;
+			
+		} catch (SQLException e) {
+			
+		}
+		
+	}
+
+	@Override
+	public void update(Department obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Department findById(Integer id) {
+		
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement("SELECT * FROM department WHERE Id = ? ");
+			
+			st.setInt(1, id);
+			
+			rs= st.executeQuery();
+			
+			if(rs.next()) {
+				Department dep = instantiateDepartment(rs);
+				
+				return dep;
+			}
+			return null;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		
+		Department dep = new Department();
+		dep.setId(rs.getInt("Id"));
+		dep.setName(rs.getString("Name"));
+		return dep;
+	}
+
+	@Override
+	public List<Department> findAll() {
+		
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		List<Department> list = new ArrayList<Department>();
+		
+		try {
+			st = conn.prepareStatement("SELECT * FROM department");
+			
+			rs = st.executeQuery();
+			
+			while(rs.next()) {
+				
+				Department dep = instantiateDepartment(rs);
+				list.add(dep);
+			}
+			
+			return list;
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+
+}
