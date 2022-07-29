8fcd246c67f4c4c326b3fe3e31d0f85ea6c5e73b
Marcos Andrade <marcos_q_andrade@hotmail.com>
2022-07-29 15:20:44 -0300

    implemented CRUD into departmentDao

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -21,6 +21,21 @@ public class Program2 {
 		List<Department> list = departmentDao.findAll();
 		list.forEach(System.out::println);
 		
+		System.out.println("\n=== TEST 3: department insert ====");
+		department = new Department(null, "Music");
+		departmentDao.insert(department);
+		System.out.println("Inserted! New Id = " + department.getId());
+		
+		System.out.println("\n=== TEST 4: department update ====");
+		department = departmentDao.findById(5);
+		department.setName("Sports");
+		departmentDao.update(department);
+		System.out.println("Update completed");
+		
+		System.out.println("\n=== TEST 5: department delete ====");
+		int id = 5;
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed");
 	}
 
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -8,6 +8,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import db.DB;
 import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -27,26 +28,80 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 		ResultSet rs = null;
 		
 		try {
-			st = conn.prepareStatement("INSERT INTO department Name VALUES (?)", 
+			st = conn.prepareStatement("INSERT INTO department (Name) VALUE (?)", 
 					Statement.RETURN_GENERATED_KEYS);
 			
-			st.setString(2, null);;
+			st.setString(1, obj.getName());
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected > 0) {
+				rs = st.getGeneratedKeys();
+				
+				if(rs.next()) {
+					int id = rs.getInt(1);
+					obj.setId(id);
+				}
+				
+			}else {
+				throw new DbException("Unexpected error! No rows affected");
+			}
 			
 		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
 			
+		}finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
 		}
 		
 	}
 
 	@Override
 	public void update(Department obj) {
-		// TODO Auto-generated method stub
+		
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement("UPDATE department SET Name = ? WHERE Id = ?");
+			
+			st.setString(1, obj.getName());
+			st.setInt(2, obj.getId());
+			
+			st.executeUpdate();
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+			
+		}finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+		
 		
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		
+		PreparedStatement st = null;
+		
+		try {
+			
+			st = conn.prepareStatement("DELETE FROM department WHERE Id = ?");
+			
+			st.setInt(1, id);
+			
+			st.executeUpdate();
+			
+		} catch (SQLException e) {
+			
+			throw new DbException(e.getMessage());
+			
+		} finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
